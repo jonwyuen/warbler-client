361b6391a022eb06a6d68139f5a6bd43c2cf1d83
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-12 00:58:51 -0800

    bootstrap nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./Nav.css";
 
 class Nav extends Component {
@@ -8,9 +9,27 @@ class Nav extends Component {
 
   render() {
     return (
-      <div className="nav-container">
-        <h1 className="nav-title">Waablr</h1>
-      </div>
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link to="/" className="navbar-brand">
+              <img
+                src="http://www.dl-digital.com/images/Birds/DD2_7688-fuzzy-titmouse.jpg"
+                alt="Warbler Home"
+              />
+              <span>Warbler</span>
+            </Link>
+          </div>
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <Link to="/signup">Sign up</Link>
+            </li>
+            <li>
+              <Link to="/signin">Log in</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
